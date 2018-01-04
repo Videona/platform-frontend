e9408692ed89b38972caa768876ffc631e003c15
Carlos Hernandez <carloshdmn@gmail.com>
2018-01-04 15:47:10 +0100

    Add register
    This commit close #3

---
+++ pages/register/controller.js
@@ -0,0 +1,59 @@
+(function() {
+
+	angular.module('app').controller('RegisterController', ['register', 'login', 'session', '$state', '$stateParams', RegisterController]);
+
+	function RegisterController(register, login, session, $state, $stateParams) {
+		var self = this;
+
+		// Service binding
+		self.service = register;
+		
+		// Properties
+		self.username = '';
+		self.email = '';
+		self.password = '';
+		self.error = '';
+		self.loading = false;
+
+		// Methods
+		self.submit = submit;
+
+
+		// On Run...
+		if(session._id > 0) {
+			console.log('Found a session! Redirecting...');
+			$state.go($stateParams.redirect || 'home');
+		}
+
+
+		// Internal functions
+		function submit() {
+			self.loading = true;
+			self.error = null;
+			console.log('Submiting...');
+			self.service.register(self.username, self.email, self.password, success);
+		}
+
+		function success(result, data) {
+			console.log(data);
+			self.loading = false;
+			if(result) {
+				self.loading = true;
+				console.log('Registered! Logging in...');
+				login.login(self.username, self.password, function(success) {
+					self.loading = false;
+					if(success) {
+						$state.go($stateParams.redirect || 'home');
+					} else {
+						self.error = 'Login error. Please, try again...';
+						$state.go('login');
+					}
+				});
+			} else {
+				console.log('Bad username, email or password...');
+				self.error = 'Your username, email or password are not good. Check it out again';
+			}
+		}
+	}
+
+})();
---
+++ pages/register/service.js
@@ -0,0 +1,42 @@
+(function(){
+
+	angular.module('app')
+		.factory('register', ['api', registerService]);
+
+	function registerService(api) {
+
+		var register = {
+			register: send,
+			pending: false
+		};
+
+		return register;
+
+
+		// Internal functions
+
+		function send(name, email, pass, cb) {
+			
+			var body = {
+				name: name,
+				email: email,
+				password: pass
+			};
+
+			register.pending = true;
+			api.post(api.url + '/user', body, function(data, status) {
+				register.pending = false;
+				var success = true;
+
+				if(status >= 400) {
+					console.error('Error while registering');
+					success = false;
+				}
+
+				cb(success, data);
+			});
+		}
+
+	}
+
+})();
