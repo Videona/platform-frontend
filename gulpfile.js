fb77bbeb7c132d519a2006691000bcc7aed5ad15
Carlos Hernandez <carloshdmn@gmail.com>
2018-01-10 11:56:58 +0100

    Create linting files and configs

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,18 @@ var cleanCSS = require('gulp-clean-css');
 var webserver = require('gulp-webserver');
 var debug = require('gulp-debug');
 var concat = require('gulp-concat');
+var eslint = require('gulp-eslint');
 var minify = require('gulp-minify');
-var watch = require('gulp-watch');
- 
+require('gulp-watch');
+
 gulp.task('serve', function () {
-  gulp.src('./')
-    .pipe(webserver({
-      host: '0.0.0.0',
-      port: 8080,
-      fallback: 'index.html',
-      livereload: true
-    }));
+	gulp.src('./')
+		.pipe(webserver({
+			host: '0.0.0.0',
+			port: 8080,
+			fallback: 'index.html',
+			livereload: true,
+		}));
 });
 
 // --------
@@ -23,7 +24,7 @@ gulp.task('serve', function () {
 // --------
 
 // Build app js
-var source_js = [
+var sourceJs = [
 	'./js/**/**.js',
 	'./languages/**/**.js',
 	'./services/**/**.js',
@@ -32,46 +33,46 @@ var source_js = [
 	'!./gulpfile.js',
 	'!./**/**.spec.js',
 	'!./node_modules/**',
-	'!./dist/**'
+	'!./dist/**',
 ];
 gulp.task('js', function () {
-	gulp.src(source_js)
-		.pipe(debug({title: 'Join JS:'}))
+	gulp.src(sourceJs)
+		.pipe(debug({ title: 'Join JS:' }))
 		.pipe(concat('app.js'))
 		.pipe(minify({
 			ext: {
 				src: '-debug.js',
-				min: '.min.js'
-			}
+				min: '.min.js',
+			},
 		}))
 		.pipe(gulp.dest('./dist/js'));
 });
 
 // Build app tests
-var source_test = [
+var sourceTest = [
 	'./test-globals.js',
 	'./**/**.spec.js',
 	'!./node_modules/**',
-	'!./dist/**'
+	'!./dist/**',
 ];
 gulp.task('tests', function () {
-	gulp.src(source_test)
-		.pipe(debug({title: 'Join tests:'}))
+	gulp.src(sourceTest)
+		.pipe(debug({ title: 'Join tests:' }))
 		.pipe(concat('test.js'))
 		.pipe(gulp.dest('./dist/js'));
 });
 
 // Move app HTML files
-var source_html = [
+var sourceHtml = [
 	'./**/**.html',
 	'!./index.html',
 	'!./_**/**',
 	'!./node_modules/**',
-	'!./dist/**'
+	'!./dist/**',
 ];
 gulp.task('html', function () {
-	gulp.src(source_html)
-		.pipe(debug({title: 'Move HTML:'}))
+	gulp.src(sourceHtml)
+		.pipe(debug({ title: 'Move HTML:' }))
 		.pipe(gulp.dest('./dist'));
 });
 
@@ -79,28 +80,41 @@ gulp.task('html', function () {
 gulp.task('build', ['js', 'html', 'tests', 'sass']);
 
 
+var sourceSass = [
+	'./pages/**/**.scss',
+];
+gulp.task('sass', function () {
+	gulp.src(sourceSass)
+		.pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+		.pipe(debug({ title: 'Move SCSS:' }))
+		.pipe(gulp.dest('./css/'));
+
+	gulp.src('./css/**/**/**.css')
+		.pipe(cleanCSS({ compatibility: 'ie8' }))
+		.pipe(concat('style.min.css'))
+		.pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('lint', function () {
+	// ESLint ignores files with "node_modules" paths.
+	// So, it's best to have gulp ignore the directory as well.
+	// Also, Be sure to return the stream from the task;
+	// Otherwise, the task may end before the stream has finished.
+	return gulp.src(['./**/**.js', '!node_modules/**', '!./_**/**', '!./dist/**'])
+	// .pipe(eslint())
+		.pipe(eslint({ fix: true }))
+		.pipe(eslint.format())
+		.pipe(gulp.dest('.'))	// <-- update original files
+		.pipe(eslint.failAfterError());
+});
+
 // Watch and build on change
 gulp.task('watch', function () {
-	gulp.watch(source_js, ['js']);
-	gulp.watch(source_test, ['tests']);
-	gulp.watch(source_html, ['html']);
-	gulp.watch(source_sass, ['sass']);
+	gulp.watch(sourceJs, ['js']);
+	gulp.watch(sourceTest, ['tests']);
+	gulp.watch(sourceHtml, ['html']);
+	gulp.watch(sourceSass, ['sass']);
 	// gulp.watch(['./src/**/*.css', './src/**/*.css'], ['styles']);
 });
 
 gulp.task('default', ['watch']);
-
-var source_sass = [
-	'./pages/**/**.scss'
-];
-gulp.task('sass', function () {
-	gulp.src(source_sass)
-		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-		.pipe(debug({title: 'Move SCSS:'}))
-		.pipe(gulp.dest('./css/'));
-	
-	gulp.src('./css/**/**/**.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(concat('style.min.css'))
-    .pipe(gulp.dest('./dist/css'));
-});
