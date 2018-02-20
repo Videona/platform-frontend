fa93cf880408b7f1434603c105575a96801ac1e2
Javier Liarte <jliarte@gmail.com>
2018-02-20 01:01:42 +0100

    Initial Jenkins setup
    - Add karma-junit-reporter dev dependency to package.json
    - Modify karma.conf.js to setup junitReporter, and change node_modules dependencies path; port change to 4200
    - Added Dockerfile.test for building test image
    - Added first Jenkinsfile setup

---
+++ Jenkinsfile
@@ -0,0 +1,58 @@
+node {
+    def app
+    def base_image = 'm4n/mojofy_frontend'
+    environment {
+        FRONTEND_TAG = 'latest'
+    }
+
+    stage('Clone repository') {
+        checkout scm
+    }
+
+
+    stage('Test image') {
+        def dockerfile = 'Dockerfile.test'
+        def testImage = docker.build("my-image:${env.BUILD_ID}", "-f ${dockerfile} ./")
+
+        testImage.inside {
+            FRONTEND_TAG = 'v' + sh (
+                script: 'node -e "console.log(require('./package.json').version);"',
+                returnStdout: true
+                ).trim()
+            sh '../node_modules/gulp/bin/gulp.js build'
+            try {
+                sh '../node_modules/karma/bin/karma start --watch false --single-run true'
+            } catch(err) {
+                sh 'echo TESTS FAILED'
+                step ([$class: 'JUnitResultArchiver', testResults: 'report/*.xml', healthScaleFactor: 1.0])
+                currentBuild.result = 'UNSTABLE'
+                throw err
+            }
+            sh 'echo "Tests passed"'
+        }
+    }
+
+
+    stage('Build image') {
+        app = docker.build("${base_image}")
+        echo "Built container image with frontend version ${env.BUILD_NUMBER}"
+    }
+
+//    stage ('Report') {
+//    //    step([$class: 'CheckStylePublisher', pattern: '**/checkstyle-result.xml'])
+//      unstash 'checkstyle-reports'
+//      step([$class: 'hudson.plugins.checkstyle.CheckStylePublisher', checkstyle: 'app/build/reports/checkstyle/checkstyle-result.xml'])
+//
+//      unstash 'junit-reports'
+//      step([$class: 'JUnitResultArchiver', testResults: '**/test-results/**/*.xml'])
+//    }
+
+
+//    stage('Push image') {
+//        docker.withRegistry('https://registry.hub.docker.com', 'docker-hub-credentials') {
+//            app.push("${env.BUILD_NUMBER}")
+//            app.push()
+//            app.push("latest")
+//        }
+//    }
+//}
--- karma.conf.js
+++ karma.conf.js
@@ -3,6 +3,12 @@
 module.exports = function (config) {
 	config.set({
 
+		plugins : [
+			'karma-chrome-launcher',
+			'karma-jasmine',
+			'karma-junit-reporter'
+		],
+
 		// base path that will be used to resolve all patterns (eg. files, exclude)
 		basePath: '',
 
@@ -15,10 +21,10 @@ module.exports = function (config) {
 		// list of files / patterns to load in the browser
 		files: [
 			// Dependencies
-			'./node_modules/angular/angular.js',
-			'./node_modules/@uirouter/angularjs/release/angular-ui-router.js',
-			'./node_modules/angular-translate/dist/angular-translate.js',
-			'./node_modules/angular-mocks/angular-mocks.js',
+			'../node_modules/angular/angular.js',
+			'../node_modules/@uirouter/angularjs/release/angular-ui-router.js',
+			'../node_modules/angular-translate/dist/angular-translate.js',
+			'../node_modules/angular-mocks/angular-mocks.js',
 			// Angular app
 			'./dist/js/app.min.js',
 			// Tests
@@ -36,15 +42,23 @@ module.exports = function (config) {
 		preprocessors: {
 		},
 
+		junitReporter: {
+			outputDir: './report', // results will be saved as $outputDir/$browserName.xml
+			outputFile: 'report.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
+			suite: '', // suite will become the package name attribute in xml testsuite element
+			useBrowserName: false, // add browser name to report and classes names
+			nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
+			classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
+			properties: {} // key value pair of properties to add to the <properties> section of the report
+		},
 
 		// test results reporter to use
 		// possible values: 'dots', 'progress'
 		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
-		reporters: ['dots', 'progress'],
-
+		reporters: ['dots', 'progress', 'junit'],
 
 		// web server port
-		port: 9876,
+		port: 4200,
 
 
 		// enable / disable colors in the output (reporters and logs)
@@ -63,7 +77,12 @@ module.exports = function (config) {
 		// start these browsers
 		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
 		browsers: ['Chrome'],
-
+		customLaunchers: {
+			Chrome_without_sandbox: {
+				base: 'Chrome',
+				flags: ['--no-sandbox'] // with sandbox it fails under Docker
+			}
+		},
 
 		// Continuous Integration mode
 		// if true, Karma captures browsers, runs the tests and exits
