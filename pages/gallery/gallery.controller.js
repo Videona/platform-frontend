e39b6479cf01d67ad06f592515ce7f9afbd3a9cf
DevStarlight <jesushuertaarrabal@gmail.com>
2018-02-22 17:56:01 +0100

    Added gallery functionality modified brand vimojo svg logo to fit the right fill color

--- pages/gallery/gallery.controller.js
+++ pages/gallery/gallery.controller.js
@@ -1,8 +1,58 @@
 angular.module('app')
-	.controller('GalleryController', ['$stateParams', 'gallery', Gallery]);
+	.controller('GalleryController', ['$stateParams', '$translate', 'gallery', Gallery]);
 
-function Gallery($stateParams, gallery) {
+function Gallery($stateParams, $translate, gallery) {
 	var self = this;
 
 	self.gallery = gallery;
+	
+	self.search = '';
+	
+	self.notFeaturedVideosQuantity = 0;
+	self.featuredWidth = 0;
+	self.notFeaturedWidth = 0;
+
+	setupFeaturedCard();
+	
+	self.featured = [];
+	self.notFeatured = [];
+
+	getFeaturedVideos();
+	getNotFeaturedVideos();
+	
+	function getFeaturedVideos() {
+		self.gallery.getFeaturedVideoList(function (data) {
+			self.featured.push(data);
+		});
+	}
+	
+	function getNotFeaturedVideos() {
+		self.gallery.getNotFeaturedVideoList(function (data) {
+			self.notFeatured.push(data);
+		});
+	}
+	
+	function setupFeaturedCard() {
+		var featuredVideoQuantity = 0;
+		var notFeaturedVideoQuantity = 0;
+		
+		if (window.innerWidth > 1920) {
+			featuredVideoQuantity = 5;
+			notFeaturedVideoQuantity = 7;
+		} else if (window.innerWidth > 1024) {
+			featuredVideoQuantity = 4;
+			notFeaturedVideoQuantity = 6;
+		} else if (window.innerWidth > 720) {
+			featuredVideoQuantity = 3.2;
+			notFeaturedVideoQuantity = 5;
+		} else {
+			featuredVideoQuantity = 2.2;
+			notFeaturedVideoQuantity = 3;
+		}
+		
+		self.featuredWidth = (window.innerWidth - parseInt(featuredVideoQuantity) * 13) / featuredVideoQuantity;
+		
+		self.notFeaturedVideosQuantity = notFeaturedVideoQuantity;
+		self.notFeaturedWidth = (window.innerWidth - parseInt(notFeaturedVideoQuantity - 1) * 13 - 26) / notFeaturedVideoQuantity;
+	}
 }
--- pages/gallery/gallery.service.js
+++ pages/gallery/gallery.service.js
@@ -1,17 +1,46 @@
 angular.module('app')
-	.service('Gallery', ['api', galleryService]);
+	.service('gallery', ['api', galleryService]);
 
 function galleryService(api) {
+	var featuredOffset = 0;
+	var notFeaturedOffset = 0;
+	
 	var gallery = {
-		get: get,
+		getFeaturedVideoList: getFeaturedVideoList,
+		getNotFeaturedVideoList: getNotFeaturedVideoList
 	};
 
 	return gallery;
 
-
-	function get() {
-		api.get(api.url + '/gallery', function (data, status) {
-			// TO-DO: Complete stuff
+	function getFeaturedVideoList(cb) {
+		var limit = 5;
+		
+		api.get(api.url + '/gallery?tag=highlighted&limit=' + limit + '&offset=' + featuredOffset, function (data, status) {
+			var success = false;
+			
+			if (status < 400) {
+				success = true;
+			}
+			
+			featuredOffset += limit;
+			
+			cb(success, data);
+		});
+	}
+	
+	function getNotFeaturedVideoList(cb) {
+		var limit = 20;
+		
+		api.get(api.url + '/gallery?excludeTag=highlighted&limit=' + limit + '&offset=' + notFeaturedOffset, function (data, status) {
+			var success = false;
+			
+			if (status < 400) {
+				success = true;
+			}
+			
+			notFeaturedOffset += limit;
+			
+			cb(success, data);
 		});
 	}
 }
