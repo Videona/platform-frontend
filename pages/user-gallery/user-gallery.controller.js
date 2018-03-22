75ccfac5ff603a7af4213868bd038f3b7ebdb9cf
DevStarlight <jesushuertaarrabal@gmail.com>
2018-03-22 15:32:35 +0100

    updated user-gallery

--- pages/user-gallery/user-gallery.controller.js
+++ pages/user-gallery/user-gallery.controller.js
@@ -7,20 +7,18 @@
 
 	function UserGallery($stateParams, $translate, userService, page) {
 		var self = this;
+		
 		self.userService = userService;
 		self.userId = $stateParams.userId;
+		self.user = {};
 
 		self.videos = {
-			videoWidth: 0,
-			videoHeight: 0,
-			videosPerRow: 0,
 			tagFilter: [],
 		};
 
-		setupVideoListDirective();
-
 		self.userService.getUser(self.userId)
-			.then((userDetails) => {
+			.then(userDetails => {
+				console.log(userDetails);
 				self.user = userDetails;
 				page.title = self.user.username + ' User gallery - ';
 				$translate('USER_GALLERY_USER_VIDEOS', { username: self.user.username })
@@ -32,22 +30,5 @@
 				console.log('Error ' + status + ' while getting user details');
 				console.log('data is ', data);
 			});
-
-		function setupVideoListDirective() {
-			self.videos.videosPerRow = 0;
-
-			if (window.innerWidth > 1920) {
-				self.videos.videosPerRow = 7;
-			} else if (window.innerWidth > 1024) {
-				self.videos.videosPerRow = 6;
-			} else if (window.innerWidth > 720) {
-				self.videos.videosPerRow = 5;
-			} else {
-				self.videos.videosPerRow = 3;
-			}
-
-			self.videos.videoWidth = (window.innerWidth - parseInt(self.videos.videosPerRow - 1) * 13 - 26) / self.videos.videosPerRow;
-			self.videos.videoHeight = (self.videos.videoWidth * 9) / 16;
-		}
 	}
 }());
