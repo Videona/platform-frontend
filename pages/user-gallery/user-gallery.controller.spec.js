2bcd9db4484a0698bb7d912e5eea10c43060ea26
Javier Liarte <jliarte@gmail.com>
2018-03-22 13:51:25 +0100

    Remove old unused services. Fix some tests
    - Remove old gallery service and page service imports from index.html
    - Remove old gallery service and gallery page service
    - Fix user gallery controller test
    - Remove ignored file css/style.css
    - Move old gallery service tests to video list component folder

--- pages/user-gallery/user-gallery.controller.spec.js
+++ pages/user-gallery/user-gallery.controller.spec.js
@@ -1,30 +1,5 @@
-const RESPONSE_SUCCESS = [{
-	date: '2018-02-27T12:47:09.762Z',
-	description: 'Description test',
-	owner: '1',
-	poster: 'http://local_cloud_storage/uploads/12b43980ebabbcddac394143082b1206.png',
-	tag: 'featured',
-	title: 'Title test',
-	video: 'http://local_cloud_storage/uploads/12b43980ebabbcddac394143082b1206.mp4',
-	_id: '20',
-}];
-
 describe('UserGalleryController', function () {
 	beforeEach(angular.mock.module('app'));
-
-	const mockedGalleryService = {
-		getVideoList: function (cb) {
-			console.log('called mocked video list......');
-			setTimeout(function () {
-				if (cb === true) {
-					cb(true, [RESPONSE_SUCCESS]);
-				} else {
-					cb(false, {});
-				}
-			}, 100);
-			return true;
-		},
-	};
 
 	beforeEach(module({
 		galleryServiceFactory: {
@@ -46,25 +21,4 @@ describe('UserGalleryController', function () {
 		expect(controller).toBeDefined();
 	});
 
-	describe('.videos.get()', function () {
-		it('should exist', function () {
-			expect(controller.videos.get).toBeDefined();
-		});
-
-		it('must be not loading', function () {
-			expect(controller.videos.isLoading).toEqual(false);
-		});
-
-		it('must have a successfull callback', function () {
-			const spy = spyOn(controller.userService, 'getUser').and.returnValue(Promise.resolve(true));
-			controller.userService.getUser();
-			expect(controller.videos.isLoading).toEqual(false);
-			//
-			spy.calls.mostRecent().returnValue.then(() => {
-				controller.videos.get();
-
-				expect(controller.videos.isLoading).toEqual(true);
-			});
-		});
-	});
 });
