df3981e5c74d71cd3d922604bfe3849e06921437
Javier Liarte <jliarte@gmail.com>
2018-03-22 15:29:39 +0100

    Fix failing video list and register tests
    - Fix video list component controller tests
    - Fix register failing test

--- components/video-list/video-list.controller.spec.js
+++ components/video-list/video-list.controller.spec.js
@@ -1,40 +1,6 @@
-describe('Gallery (video api) factory', function () {
-	let galleryFactory;
-
-	beforeEach(angular.mock.module('app'));
-	beforeEach(mockApi);
-	beforeEach(inject(function (_galleryServiceFactory_) {
-		galleryFactory = _galleryServiceFactory_;
-	}));
-
-
-	it('should exist', function () {
-		expect(galleryFactory).toBeDefined();
-	});
-
-	describe('service instance', function () {
-		it('should exist', function () {
-			expect(galleryFactory.getInstance).toBeDefined();
-		});
-
-		it('should have no videos before calling the callback', function () {
-			const gallery = galleryFactory.getInstance();
-			expect(gallery.videos.length).toBe(0);
-		});
-
-		it('should have default query params limit of 10 items', function () {
-			const gallery = galleryFactory.getInstance();
-			expect(gallery.queryParams.limit).toEqual(10);
-			expect(gallery.queryParams.offset).toEqual(0);
-			expect(gallery.queryParams.order).toEqual('-date');
-			expect(gallery.queryParams.tag).toBeUndefined();
-			expect(gallery.queryParams.excludeTag).toBeUndefined();
-		});
-	});
-});
-
 function mockApi() {
-	mockedApi = {
+	return {
+		url: 'here',
 		get: function (uri, cb) {
 			setTimeout(function () {
 				cb(true, {});
@@ -44,8 +10,10 @@ function mockApi() {
 	};
 }
 
-describe('UserGallery (video api) service', function () {
-	const mockedApi = null;
+describe('VideoListController', function () {
+	beforeEach(angular.mock.module('app'));
+
+	let mockedApi = null;
 	let callback = null;
 	let serializerSpy = null;
 
@@ -56,79 +24,115 @@ describe('UserGallery (video api) service', function () {
 		serializerSpy = jasmine.createSpy();
 	}
 	beforeEach(createCallbackSpy);
-	beforeEach(mockApi);
+	beforeEach(function () {
+		mockedApi = mockApi();
+	});
 	beforeEach(createSerializerSpy);
 
-	it('does not set tag filters if no filters param passed', function () {
-		const service = new GalleryService(mockedApi);
+	// let videoListCtrl;
+	let $componentController;
 
-		expect(service.queryParams.tag).toBeUndefined();
-		expect(service.queryParams.excludeTag).toBeUndefined();
+	beforeEach(inject(function (_$componentController_) {
+		$componentController = _$componentController_;
+	}));
+
+
+	function getVideoListController(userId, tagFilter) {
+		return $componentController('videoList',
+			{api: mockedApi, $httpParamSerializer: serializerSpy},
+			{
+				// bindings
+				userId: userId,
+				tagFilter: tagFilter,
+				infiniteScrollEnabled: false,
+			});
+	}
+
+	function getVideoListControllerWithoutUser(tagFilter) {
+		return $componentController('videoList',
+			{api: mockedApi, $httpParamSerializer: serializerSpy},
+			{
+				// bindings
+				tagFilter: tagFilter,
+				infiniteScrollEnabled: false,
+			});
+	}
+
+	it('does not set tag filters if no filters param passed', function () {
+		const videoListCtrl = getVideoListController(0, []);
+
+		expect(videoListCtrl.queryParams.tag).toBeUndefined();
+		expect(videoListCtrl.queryParams.excludeTag).toBeUndefined();
 	});
 
 	it('set user if int userId is provided', function () {
-		const service = new GalleryService(mockedApi, serializerSpy, [], 42);
+		const videoListCtrl = getVideoListController(42, []);
 
-		expect(service.userId).toBe(42);
+		expect(videoListCtrl.userId).toBe(42);
 	});
 
 	it('does not set user if non int userId is provided', function () {
-		const service = new GalleryService(mockedApi, serializerSpy, [], '');
+		const videoListCtrl = getVideoListController(undefined, []);
 
-		expect(service.userId).toBeUndefined();
+		expect(videoListCtrl.userId).toBeUndefined();
 	});
 
 
 	describe('.createTagFilter', function () {
 		it('should exists', function () {
-			const service = new GalleryService(mockedApi, serializerSpy, []);
+			const videoListCtrl = getVideoListController(0, []);
 
-			expect(service.createTagFilter).toBeDefined();
+			expect(videoListCtrl.createTagFilter).toBeDefined();
 		});
 
 		it('should be called in constructor with tags param', function () {
 			const tagFilter = ['oneTag'];
-			const createTagFilterSpy = spyOn(GalleryService.prototype, 'createTagFilter');
+			const videoListCtrl = getVideoListController(0, tagFilter);
+			const createTagFilterSpy = spyOn(videoListCtrl, 'createTagFilter');
 
-			const service = new GalleryService(mockedApi, serializerSpy, tagFilter);
+			videoListCtrl.$onInit();
 
 			expect(createTagFilterSpy).toHaveBeenCalled();
 		});
 
 		it('should set tag filter with tags passed', function () {
 			const tagFilter = ['oneTag', 'anotherTag'];
+			const videoListCtrl = getVideoListController(0, tagFilter);
 
-			const service = new GalleryService(mockedApi, serializerSpy, tagFilter);
+			videoListCtrl.$onInit();
 
-			expect(service.queryParams.tag).toEqual(['oneTag', 'anotherTag']);
+			expect(videoListCtrl.queryParams.tag).toEqual(['oneTag', 'anotherTag']);
 		});
 
 		it('should set excludeTag filter with tags passed with a -', function () {
 			const tagFilter = ['oneTag', 'anotherTag', '-excludedTag'];
+			const videoListCtrl = getVideoListController(0, tagFilter);
 
-			const service = new GalleryService(mockedApi, serializerSpy, tagFilter);
+			videoListCtrl.$onInit();
 
-			expect(service.queryParams.tag).toEqual(['oneTag', 'anotherTag']);
-			expect(service.queryParams.excludeTag).toEqual(['excludedTag']);
+			expect(videoListCtrl.queryParams.tag).toEqual(['oneTag', 'anotherTag']);
+			expect(videoListCtrl.queryParams.excludeTag).toEqual(['excludedTag']);
 		});
 	});
 
 	describe('getQuery', function () {
 		it('should exist', function () {
-			const service = new GalleryService(mockedApi, serializerSpy);
+			const videoListCtrl = getVideoListController(0, []);
 
-			expect(service.getQuery).toBeDefined();
+			expect(videoListCtrl.getQuery).toBeDefined();
 		});
 
 		it('should call $httpParamSerializer', function () {
-			const service = new GalleryService(mockedApi, serializerSpy, ['tag1', '-tag2', 'tag3']);
+			let user;
+			const videoListCtrl = getVideoListController(user, ['tag1', '-tag2', 'tag3']);
+			videoListCtrl.$onInit();
 
-			const query = service.getQuery();
+			const query = videoListCtrl.getQuery();
 
 			expect(serializerSpy).toHaveBeenCalledWith({
-				order: service.queryParams.order,
-				offset: service.queryParams.offset,
-				limit: service.queryParams.limit,
+				order: videoListCtrl.queryParams.order,
+				offset: videoListCtrl.queryParams.offset,
+				limit: videoListCtrl.queryParams.limit,
 				tag: ['tag1', 'tag3'],
 				excludeTag: ['tag2'],
 			});
@@ -136,9 +140,10 @@ describe('UserGallery (video api) service', function () {
 
 		it('defines /videos as query base', function () {
 			serializerSpy = jasmine.createSpy().and.returnValue('queryparams');
-			const service = new GalleryService(mockedApi, serializerSpy);
+			const videoListCtrl = getVideoListControllerWithoutUser([]);
+			videoListCtrl.$onInit();
 
-			const query = service.getQuery();
+			const query = videoListCtrl.getQuery();
 
 			// expect(query).toStartWith('/video');
 			expect(query).toBe('/video?queryparams');
@@ -146,9 +151,9 @@ describe('UserGallery (video api) service', function () {
 
 		it('prepends user if userId is provided', function () {
 			serializerSpy = jasmine.createSpy().and.returnValue('queryparams');
-			const service = new GalleryService(mockedApi, serializerSpy, [], 42);
+			const videoListCtrl = getVideoListController(42, []);
 
-			const query = service.getQuery();
+			const query = videoListCtrl.getQuery();
 
 			// expect(query).toStartWith('/user/42');
 			expect(query).toBe('/user/42/video?queryparams');
