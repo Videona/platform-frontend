e037bf275c6e1981532290f78ccefc5b23296e1f
Javier Liarte <jliarte@gmail.com>
2018-03-28 10:57:20 +0200

    Handle userId param not defined or not number in user gallery controller

--- pages/user-gallery/user-gallery.controller.js
+++ pages/user-gallery/user-gallery.controller.js
@@ -17,20 +17,28 @@
 			tagFilter: [],
 		};
 
-		self.userService.getUser(self.userId)
-			.then(userDetails => {
-				self.user = userDetails;
-				if (typeof self.user !== 'undefined') {
-					page.title = self.user.username + ' ' + page.title;
-					$translate('USER_GALLERY_USER_VIDEOS', { username: self.user.username })
-						.then(title => self.userGalleryTitle = title);
-				}
-			})
-			.catch((status, data) => {
-				page.title = 'User gallery - ';
-				// TODO(jliarte): 404?
-				console.log('Error ' + status + ' while getting user details');
-				console.log('data is ', data);
-			});
+		function handleNoUserError() {
+			page.title = 'User gallery - ';
+			// TODO(jliarte): 404?
+		}
+
+		if (self.userId !== undefined && !isNaN(parseInt(self.userId))) {
+			self.userService.getUser(self.userId)
+				.then(userDetails => {
+					self.user = userDetails;
+					if (typeof self.user !== 'undefined') {
+						page.title = self.user.username + ' ' + page.title;
+						$translate('USER_GALLERY_USER_VIDEOS', { username: self.user.username })
+							.then(title => self.userGalleryTitle = title);
+					}
+				})
+				.catch((status, data) => {
+					handleNoUserError();
+					console.log('Error ' + status + ' while getting user details');
+					console.log('data is ', data);
+				});
+		} else {
+			handleNoUserError();
+		}
 	}
 }());
