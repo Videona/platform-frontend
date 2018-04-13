4e6be28897838642454d924886baccf2d7fa75c2
Javier Liarte <jliarte@gmail.com>
2018-04-13 14:01:01 +0200

    Add missing ngMap deppendency to app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 (function () {
 	// App
 	angular.module('app', ['app.config', 'app.flavour', 'ui.router', 'pascalprecht.translate', 'infinite-scroll',
-		'ngFileUpload', 'angularMoment', 'ngMaterial'])
+		'ngFileUpload', 'angularMoment', 'ngMaterial', 'ngMap'])
 		.config(['$locationProvider', '$stateProvider', '$urlRouterProvider', '$translateProvider', '$mdThemingProvider',
 			'$mdDateLocaleProvider', conf]);
 
