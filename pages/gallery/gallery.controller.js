e2338abb8d24b4b041deeef81d71d2b1aa90291c
CodingCarlos <carloshdmn@gmail.com>
2018-05-03 16:18:03 +0200

    First approach

--- pages/gallery/gallery.controller.js
+++ pages/gallery/gallery.controller.js
@@ -1,8 +1,23 @@
 angular.module('app')
-	.controller('GalleryController', ['$translate', Gallery]);
+	.controller('GalleryController', ['$scope', '$document', '$translate', Gallery]);
 
-function Gallery($translate) {
+function Gallery($scope, $document, $translate) {
 	var self = this;
 
 	self.search = '';
+	self.topbarTransparent = true;
+
+	// Add a scroll listener to enable the header+topbar effect
+	$document.on('scroll', scrolling)
+
+	// Listen controller destroy to remove scroll listener
+	$scope.$on("$destroy", function() {
+		$document.off('scroll', scrolling)
+	});
+
+	function scrolling(e) {
+		var offset = 56;
+		var header = document.getElementsByClassName('header-box')[0];
+		self.topbarTransparent = window.scrollY < (header.scrollHeight - offset);
+	}
 }
