73f90653430461c5fabba5e11e2de5564589b971
CodingCarlos <carloshdmn@gmail.com>
2018-06-13 16:22:21 +0200

    Add progress bar to move to FTP

--- components/distribute/distribution.controller.js
+++ components/distribute/distribution.controller.js
@@ -1,7 +1,7 @@
 angular.module('app')
-	.controller('DistributionController', ['$mdDialog', '$mdToast', '$translate', 'distribute', '$stateParams', DistributionController]);
+	.controller('DistributionController', ['$interval', '$mdDialog', '$mdToast', '$translate', 'distribute', '$stateParams', DistributionController]);
 
-function DistributionController($mdDialog, $mdToast, $translate, distribute, $stateParams) {
+function DistributionController($interval, $mdDialog, $mdToast, $translate, distribute, $stateParams) {
 	var self = this;
 
 	self.send = send;
@@ -15,6 +15,9 @@ function DistributionController($mdDialog, $mdToast, $translate, distribute, $st
 	self.hideAddClient = function () {
 		self.status.isAddingClient = false;
 	}
+	self.distributing = 0;
+	self.progress = 0;
+	self.progressing = {};
 
 	distribute.get($stateParams.id, function (data) {
 		console.log(data);
@@ -30,22 +33,83 @@ function DistributionController($mdDialog, $mdToast, $translate, distribute, $st
 			return false;
 		}
 
+		self.distributing = clientList.length;
+		self.progress = 0;
+		self.progressing = {};
+
 		for (var i = 0; i < clientList.length; i++) {
-			console.log($stateParams.id, clientList[i]._id)
-			distribute.add($stateParams.id, clientList[i]._id, function (addData) {
-				console.log(addData);
-				if(++results === clientList.length) {
-					self.loading = false;
-					self.hide();
-					var message = $translate.instant('DISTRIBUTE_DONE');
-					$mdToast.show(
-						$mdToast.simple()
-							.textContent(message)
-							.toastClass("mojofy-toast")
-							.position("fixed-top right")
-					);
-				}
-			});
+
+			(function () {
+				var client = clientList[i];
+				console.log('Fake progress for client:');
+				console.log(client);
+				fakeProgress(client._id);
+
+				distribute.add($stateParams.id, clientList[i]._id, function (addData) {
+
+					console.log(self.progress);
+					console.log('STOP Fake progress for client:');
+					console.log(client);
+					stopProgress(client._id);
+
+					if(++results === clientList.length) {
+						self.loading = false;
+						self.hide();
+						var message = $translate.instant('DISTRIBUTE_DONE');
+						$mdToast.show(
+							$mdToast.simple()
+								.textContent(message)
+								.toastClass("mojofy-toast")
+								.position("fixed-top right")
+						);
+					}
+				});
+			})();
 		}
+	}
+
+	function fakeProgress(id) {
+		var maxProgress = 100 / self.distributing;
+		var maxExpectedTime = 90;
+
+		// Init the progress
+		self.progressing[id] = { id: id, value: 1 };
+
+		// Prepare update interval
+		self.progressing[id].interval = $interval(function () {
+			var rand = Math.floor(Math.random() * 10);
+			var shallUpdate = (rand % 2 === 0);
+			console.log('---------------')
+			console.log(self.progressing[id].value)
+			console.log(id)
+			console.log('---------------')
+			if(self.progressing[id].value < maxProgress && shallUpdate) {
+				self.progressing[id].value += maxProgress / maxExpectedTime;
+				self.progress += self.progressing[id].value;
+			}
+		}, 1000);
+
+		// Update progress with initial value
+		self.progress += self.progressing[id].value;
+	}
+
+
+	function stopProgress(id) {
+		console.log('Stop progpagating');
+		var maxProgress = 100 / self.distributing;
+
+		// Stop interval
+		$interval.cancel(self.progressing[id].interval);
+		self.progressing[id].interval = undefined;
+
+		// Update global prorgess with remaining for that request
+		var remaining = maxProgress - self.progressing[id].value;
+		console.log(remaining);
+		console.log(self.progressing[id]);
+		self.progress += remaining
+
+		// Update progress value for the request
+		self.progressing[id].value = maxProgress;
+		console.log(self.progressing[id]);
 	}
 }
