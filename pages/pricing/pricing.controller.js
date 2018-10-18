a52b1f60c204cd4032292bf25248d9aaab583e8a
javierLiarte <jliarte@gmail.com>
2018-10-18 13:34:34 +0200

    Add responsive design to pricing page
    - Add responsive design to pricing.view
    - Update pricing controller with some helper functions to handle with responsive steps

--- pages/pricing/pricing.controller.js
+++ pages/pricing/pricing.controller.js
@@ -1,21 +1,71 @@
 angular.module('app')
 	.controller('PricingController', ['$mdToast', '$scope', '$translate', 'page', '$location', '$anchorScroll',
-		'pricing', '$state', '$analytics', 'session', PricingController]);
+		'pricing', '$state', '$analytics', 'session', '$mdMedia', '$scope', PricingController]);
 
 function PricingController($mdToast, $scope, $translate, page, $location, $anchorScroll, pricing, $state, $analytics,
-                           session) {
+                           session, $mdMedia, $scope) {
 	var self = this;
+	$scope.$watch(function() { return $mdMedia('gt-sm'); }, function(big) {
+		$scope.smallScreen = !big;
+		console.log("small screen is ", !big);
+		if (big) {
+			self.showTableProduct();
+		} else {
+			self.showTableProduct('free');
+		}
+	});
+
 	page.setPageTitle($translate.instant('PRICING_PAGE_WEB_TITLE'));
 
-	let selectedPlan;
+	let selectedProduct;
 	self.plans = pricing.getPlans();
 	self.featureTable = pricing.getFeatureTable();
 	self.featureTableProducts = [ 'free', 'witness', 'journalist', 'hero', 'superHero' ];
 
-	// selfish Methods
+	self.showTableHeader = true;
+	self.showTableColumn = {
+		free: true,
+		witness: true,
+		journalist: true,
+		hero: true,
+		superHero: true
+	};
+
+	function hideAllProductsInTable() {
+		for (let idx in self.showTableColumn) {
+			self.showTableColumn[idx] = false;
+		}
+	}
+
+	function showFullProductTable() {
+		for (let idx in self.showTableColumn) {
+			self.showTableColumn[idx] = true;
+		}
+		self.showTableHeader = true;
+	}
+
+// selfish Methods
+	self.showTableProduct = function (productId) {
+		if (productId) {
+			selectedProduct = productId;
+			hideAllProductsInTable();
+			self.showTableHeader = false;
+			self.showTableColumn[productId] = true;
+		} else {
+			selectedProduct = undefined;
+			showFullProductTable();
+		}
+	};
+
+	self.getClassForProductButton = function (productId) {
+		if (productId == selectedProduct) {
+			return 'product-selected';
+		}
+	};
+
 	self.expandPlanInfo = function (productId) {
 		console.log("request more info of productId ", productId);
-		selectedPlan = productId;
+		selectedProduct = productId;
 		$analytics.eventTrack('Product Detail', {  productId: productId });
 
 		$location.hash('feature-table-comparison');
